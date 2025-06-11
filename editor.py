567c787b5f0c0f00c72baf48b53309601ebaf201
Ravi Chhawal <ravichhawal@hotmail.com>
2025-06-11 16:39:06 +0530

    16:40 evening commit at office

--- editor.py
+++ editor.py
@@ -7,7 +7,7 @@ from scripts.tilemap import Tilemap
 RENDER_SCALE = 2.0
 
 
-# https://youtu.be/2gABYM5M0ww?t=9371
+# https://youtu.be/2gABYM5M0ww?t=9434
 class Editor:
     def __init__(self):
         pygame.init()
@@ -38,14 +38,17 @@ class Editor:
         self.tile_group = 0
         self.tile_variant = 0
 
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
+
     def run(self):
 
         while True:
             self.display.fill((0, 0, 0))
 
-            current_tile_img = self.assets[self.tile_list[self.tile_group]][
-                self.tile_variant
-            ].copy()
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy(
+            )
             current_tile_img.set_alpha(160)
 
             self.display.blit(current_tile_img, (5, 5))
@@ -60,33 +63,50 @@ class Editor:
                         self.clicking = True
                     if event.button == 3:
                         self.right_clicking = True
-                    if event.button == 4:
-                        self.tile_group = (self.tile_group - 1) % len(self.tile_list)
-                    if event.button == 5:
-                        self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                    if self.shift:
+                        if event.button == 4:
+                            self.tile_variant = (
+                                self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])
+                        if event.button == 5:
+                            self.tile_variant = (
+                                self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (
+                                self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0
+
+                        if event.button == 5:
+                            self.tile_group = (
+                                self.tile_group + 1) % len(self.tile_list)
+                            self.tile_variant = 0
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
-                        self.movement[-0] = True
+                        self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.movement[1] = True
+                        self.movement[2] = True
                     if event.key == pygame.K_DOWN:
-                        self.movement[1] = True
-
+                        self.movement[3] = True
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
                     if event.key == pygame.K_UP:
-                        self.movement[1] = False
+                        self.movement[2] = False
                     if event.key == pygame.K_DOWN:
-                        self.movement[1] = False
+                        self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT:
+                        self.shift = False
 
             self.screen.blit(
-                pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)
+                pygame.transform.scale(
+                    self.display, self.screen.get_size()), (0, 0)
             )
             # self.screen.blit(textSurfaceObj, textRectObj)
 
