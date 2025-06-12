004c9fad067a9ba9bee6fd1bb1047b2cc64ae176
Ravi Chhawal <ravichhawal@hotmail.com>
2025-06-12 15:07:23 +0530

    before Lunch Break

--- editor.py
+++ editor.py
@@ -47,11 +47,29 @@ class Editor:
         while True:
             self.display.fill((0, 0, 0))
 
+            render_scrool = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset=render_scrool)
+
             current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy(
             )
             current_tile_img.set_alpha(160)
 
-            self.display.blit(current_tile_img, (5, 5))
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0]/RENDER_SCALE, mpos[1]/RENDER_SCALE)
+            tile_pos = (int(mpos[0]+self.scroll[0])//self.tilemap.tile_size,
+                        int(mpos[1]+self.scroll[1])//self.tilemap.tile_size)
+
+            if self.clicking:
+                self.tilemap.tilemap[str(tile_pos[0])+';'+str(tile_pos[1])] = {
+                    'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
+            if self.right_clicking:
+                tile_loc = str(tile_pos[0])+';'+str(tile_pos[1])
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
+
+            self.display.blit(
+                current_tile_img, (self.tile_pos[0]*self.tilemap.tile_size-self.scroll[0], self.tile_pos[1]*self))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -80,6 +98,12 @@ class Editor:
                             self.tile_group = (
                                 self.tile_group + 1) % len(self.tile_list)
                             self.tile_variant = 0
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.clicking = False
+                    if event.button == 3:
+                        self.right_clicking = False
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
